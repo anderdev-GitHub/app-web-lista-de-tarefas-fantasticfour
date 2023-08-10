51832be3adbbe4d715500252d0a88ffaa1afca29
Marcos Aurélio <126178612+Marcosaurelio36@users.noreply.github.com>
2023-08-10 12:26:55 -0300

    Istarting functions

---
+++ script.js
@@ -0,0 +1,22 @@
+  const button= document.querySelector('#btn-new-task')
+  const input =document.querySelector('#input-new-task')
+  const task= document.querySelector('to-do-list')
+
+
+
+
+function  addNewTask (){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+  button.addEventListener ('click',addNewTask)
